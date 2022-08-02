01aee55bf0571c8c37f9a197d174e192253493b0
CarlosR8 <carlos.rivera8528@hotmail.com>
2022-08-02 02:31:07 +0200

    Added gaming layer

--- keyboards/planck/keymaps/cmrivera/keymap.c
+++ keyboards/planck/keymaps/cmrivera/keymap.c
@@ -28,6 +28,7 @@ int language = ES;
 // Layers
 enum planck_layers {
   _COLEMAK,
+  _GAMING,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -36,6 +37,7 @@ enum planck_layers {
 
 enum planck_keycodes {
   COLEMAK = SAFE_RANGE,
+  GAMING,
   BACKLIT,
   EXT_PLV,
 };
@@ -227,7 +229,7 @@ void dance_mouse_tap(qk_tap_dance_state_t *state, void *user_data) {
 void dance_mouse_finish(qk_tap_dance_state_t *state, void *user_data) {
 keytap_state.state = cur_dance(state);
     switch (keytap_state.state) {
-        case TD_DOUBLE_HOLD: layer_on(_MOUSE); break; 
+        case TD_DOUBLE_HOLD: layer_on(_MOUSE); break;
         default: break;
     }
 }
@@ -727,6 +729,24 @@ KC_LSFT     ,M(M_Z)         ,KC_X           ,TD(TD_C)     ,KC_V         ,KC_B   
 KC_LCTL     ,RGB_TOG        ,KC_LGUI        ,KC_LALT      ,LOWER        ,KC_SPC  ,KC_SPC  ,TD(TD_MOUSE)  ,KC_APP       ,KC_DOWN      ,KC_UP        ,KC_RGHT    
 ),
 
+/* Gaming (Colemak without tap dancing)
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  ' " |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | RGB  | GUI  | Alt  |Lower |    Space    |Raise | MENU | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_GAMING] = LAYOUT_planck_grid(
+KC_ESC      ,KC_Q           ,KC_W           ,KC_F         ,KC_P         ,KC_G    ,KC_J    ,KC_L          ,KC_U         ,KC_Y         ,KC_SCLN      ,KC_BSPC, 
+KC_TAB      ,KC_A           ,KC_R           ,KC_S         ,KC_T         ,KC_D    ,KC_H    ,KC_N          ,KC_E         ,KC_I         ,KC_O         ,KC_QUOT,
+KC_LSFT     ,KC_Z           ,KC_X           ,KC_C         ,KC_V         ,KC_B    ,KC_K    ,KC_M          ,KC_COMM      ,KC_DOT       ,KC_SLSH      ,KC_ENT ,
+KC_LCTL     ,RGB_TOG        ,KC_LGUI        ,KC_LALT      ,LOWER        ,KC_SPC  ,KC_SPC  ,TD(TD_MOUSE)  ,KC_APP       ,KC_DOWN      ,KC_UP        ,KC_RGHT    
+),
+
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   (  |   )  |   %  |   ^  |   7  |   8  |   9  |   -  | Bksp |
@@ -769,7 +789,7 @@ _______ ,_______      ,_______      ,_______      ,_______      ,_______ ,______
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|  ES  |  EN  |  FR  |Plover|      |
+ * |      |      |MUSmod|Aud on|Audoff|      |      |  ES  |  EN  |  FR  |Colema|Gaming|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -778,7 +798,7 @@ _______ ,_______      ,_______      ,_______      ,_______      ,_______ ,______
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, M(M_ES),  M(M_EN),  M(M_FR), _______, _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, M(M_ES),  M(M_EN),  M(M_FR), COLEMAK, GAMING,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 ),
@@ -813,6 +833,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // set_single_persistent_default_layer(_COLEMAK);
   switch (keycode) {
     // case QWERTY:
     //   if (record->event.pressed) {
@@ -833,12 +854,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // }
     // return false;
     // break;
-    // case DVORAK:
-    //   if (record->event.pressed) {
-    //     set_single_persistent_default_layer(_DVORAK);
-    //   }
-    //   return false;
-    //   break;
+    case GAMING:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_GAMING);
+      }
+      return false;
+      break;
     case BACKLIT:
       if (record->event.pressed) {
         register_code(KC_RSFT);
